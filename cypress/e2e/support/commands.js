44bd63943d5e47ccef934dca5bec511c9aeddb05
Tyron Fonseca <tyron.fonseca@hotmail.com>
2022-11-20 10:45:01 -0600

    Added test cases for Login and small refactoring
    -Added command to verify Toast messages
    -Change location of commands for login and logout

--- cypress/e2e/support/commands.js
+++ cypress/e2e/support/commands.js
@@ -1,24 +1,8 @@
-// Expects to be in login page
-Cypress.Commands.add('login', () => {
-    cy.visit(Cypress.env("login_url"));
-        cy.get("input[name='email']").type(Cypress.env("testUser").email);
-        cy.get("input[name='password']").type(Cypress.env("testUser").pwd);
-        
-        let btn_login = cy.get("ion-button").should("contain", "Iniciar sesión");
-        btn_login.click({ multiple: true });
-});
-
-Cypress.Commands.add('logout', () => {
-    cy.visit('/edit-information');
-    cy.wait(1000);
-    let btn_logout = cy.get('ion-button').last();
-    btn_logout.click({ force: true });
-});
-
+// Include this file if you want to use custom commands or overwrite existing commands
 beforeEach(() => {
-    cy.login()
+  cy.login();
 });
 
 afterEach(() => {
-    cy.logout()
+  cy.logout();
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Expects to be in login page
+Cypress.Commands.add("login", () => {
+  cy.visit(Cypress.env("login_url"));
+  cy.get("input[name='email']").type(Cypress.env("testUser").email);
+  cy.get("input[name='password']").type(Cypress.env("testUser").pwd);
+
+  let btn_login = cy.get("ion-button").should("contain", "Iniciar sesión");
+  btn_login.first().click();
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.visit("/edit-information");
+  cy.wait(5000);
+  let btn_logout = cy.get("ion-button").last();
+  btn_logout.click({ force: true });
+});
+
+Cypress.Commands.add("verifyToastMsg", (message) => {
+  cy.get("ion-toast")
+    .should("exist")
+    .shadow()
+    .contains(".toast-message", message);
+});
